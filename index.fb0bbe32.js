156c785a430a214931db81f48ebae90908748ffc
Sergiy-Shevchenko <116056626+Sergiy-Shevchenko@users.noreply.github.com>
2023-11-10 13:37:11 +0000

    Deploying to gh-pages from @ Sergiy-Shevchenko/notar@c31b0b81f7fe283d1f1279b215dc976609d3c210 🚀

--- index.fb0bbe32.js
+++
@@ -1,2 +0,0 @@
-const i=[{id:"id-1",name:"Наші переваги"},{id:"id-2",name:"Наші послуги"},{id:"id-3",name:"Наш офіс"},{id:"id-4",name:"Наш графік"}],n=document.querySelector(".list");!function(){const e=i.map((({id:i,name:n})=>`\n    <li class="item">\n    <a class="link" href="#${i}">${n}</a>\n    </li> `)).join();n.insertAdjacentHTML("afterbegin",e)}();
-//# sourceMappingURL=index.fb0bbe32.js.map
