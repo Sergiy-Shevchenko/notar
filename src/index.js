6fed641a4b6998481b4c225215a3833d9340f58b
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-11-12 20:04:51 +0200

    js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Report } from 'notiflix/build/notiflix-report-aio'
 import { servise } from './components/servise';
-import check from './images/check_1.png'
+// import check from './images/check_1.png'
 
 // console.log(check);
 // import 
